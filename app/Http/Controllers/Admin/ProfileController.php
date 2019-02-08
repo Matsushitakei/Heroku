f41c22cf0c35a30d62700425dfe510d181aa26da
松下慧 <matsushitakei@K1212.local>
2019-02-08 13:40:58 +0900

    laravel kadai17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -19,14 +19,39 @@ class ProfileController extends Controller
       //Varidationを行う
       $this->validate($request, Profile::$rules);
 
-      $profiles = new Profile;
+      $profile = new Profile;
       $form = $request->all();
 
+      unset($form['token']);
       //データベースに保存する
-      $profiles->fill($form);
-      $profiles->save();
+      $profile->fill($form);
+      $profile->save();
 
-      // admin/profile/editにリダイレクトする
+      // admin/profile/createにリダイレクトする
       return redirect('admin/profile/create');
+    }
+
+    public function edit(Request $request)
+    {
+      //Profile Modelからデータを取得する
+      $profile = Profile::orderBy('created_at', 'desc')->first();
+
+      return view('admin.profile.edit',['profile_form' => $profile]);
+    }
+
+    public function update(Request $request)
+    {
+      //Validationをかける
+      $this->validate($request, Profile::$rules);
+      //profile Modelからデータを取得する
+      $profile = new Profile;
+      //送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+
+      //該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile/edit');
     }
 }
